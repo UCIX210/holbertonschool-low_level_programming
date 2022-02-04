f7a6bbfd6f3d5b0fe9839f4a9a8c82e98af3110a
UCIX210 <javier.uc@outlook.com>
2022-02-03 23:26:57 -0600

    Ox00 error1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - function main
  * Return: 1 always
  */
-
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
