a81d1ca31e2638804d81fec29c027ae678c07586
UCIX210 <javier.uc@outlook.com>
2022-02-04 20:22:41 -0600

    Ox01 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,29 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - random numbers, negative or positive
+ * main - prints alphabet in order
  *
- * Return: 0 Always
+ * Return: Always 0.
  */
 int main(void)
 {
-	int n;
+	char l;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
+	for (l = 'a'; l <= 'z'; l++)
 	{
-		printf("%d is positive\n", n);
+		putchar(l);
 	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-
+	putchar('\n');
 	return (0);
 }
