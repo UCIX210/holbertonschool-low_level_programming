c6531dc6cb343ca82f2d11fa6d65786d3436ae82
UCIX210 <javier.uc@outlook.com>
2022-02-04 21:18:15 -0600

    0x01 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,30 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - random numbers, negative or positive
- *
- * Return: 0 Always
- */
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
+	for (n = '0'; n <= '9'; n++)
 	{
-		printf("%d is positive\n", n);
+		putchar(n);
 	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+	putchar('\n');
 
 	return (0);
 }
