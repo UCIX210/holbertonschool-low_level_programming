ca15906389a0c8ccec9967acc51bf956a0faf82e
UCIX210 <javier.uc@outlook.com>
2022-02-08 20:27:47 -0600

    0x02 3.1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - prints alphabet
+ * _islower - minuscula
  *
- * Return: 0 Always
+ * Return: 0 == Mayuscula 1==minuscula
  */
-int main(void)
+int _islower(int c)
 {
-
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
