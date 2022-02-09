4e7cce4cd1115070f9965c4d96851e9580322f6a
UCIX210 <javier.uc@outlook.com>
2022-02-08 23:35:57 -0600

    0x02 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 /**
-  * main - 0 to 9 with ','
+  * times_table - multiplicas
   *
-  * Return: Always (Success);
+  * Return: void
   */
-int main(void)
+void times_table(void)
 {
+	int a;
+	int b;
+	int c;
 
+	for (a = 0; a <= 9; a++)
+	{
+		putchar('0');
+		for (b = 1; b <= 9; b++)
+		{
+			c = a * b;
+			putchar(',');
+			putchar(' ');
+			if (c <= 9)
+			{
+				putchar(' ');
+		}
+			else
+			{
+				putchar((c / 10) + '0');
+			}
+			putchar((c % 10) + '0');
+		}
+		putchar('\n');
+	}
 }
