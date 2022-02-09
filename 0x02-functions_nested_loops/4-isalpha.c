11c6b0f611a5cbd9eca4dace265194b82f62831d
UCIX210 <javier.uc@outlook.com>
2022-02-08 20:45:03 -0600

    0x02 4.1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - prints alphabet -o and -q
- *
- * Return: Always 0.
+ * _isalpha - letra
+ * @c: variable a chequear
+ * Return:  1==letra else - 0
  */
-int main(void)
+int _isalpha(int c)
 {
-
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
