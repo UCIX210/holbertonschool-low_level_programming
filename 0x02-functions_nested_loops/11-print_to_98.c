532e9fd587c7422e6079ceed7741287d860d5c9c
UCIX210 <javier.uc@outlook.com>
2022-02-08 23:53:21 -0600

    0x02 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,25 @@
+#include "main.h"
 #include <stdio.h>
 /**
-  * main - 0 to 9 with ','
-  *
-  * Return: Always (Success);
-  */
-int main(void)
+ * print_to_98 - 98 magico
+ * @n: numero
+ * Return: void
+ */
+void print_to_98(int n)
 {
-
-}
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%i, ", n--);
+		}
+		printf("%i\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%i, ", n++);
+		}
+		printf("%i\n", n);
+	}
