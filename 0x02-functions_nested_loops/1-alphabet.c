e8b79e6baf5a5a714e8e825ffeddf22ad86c1ee2
UCIX210 <javier.uc@outlook.com>
2022-02-08 19:37:07 -0600

    Ox02 1.3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,10 @@
  */
 void print_alphabet(void)
 {
-    char i;
-
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
-
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
 }
