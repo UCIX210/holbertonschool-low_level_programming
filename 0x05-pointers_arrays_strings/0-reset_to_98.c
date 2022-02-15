b996c68aa73137d831dc358e5be612b5c175fd13
UCIX210 <javier.uc@outlook.com>
2022-02-14 18:57:43 -0600

    0x05 0.1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - Checks for uppercase charactertakes a pointer to an
- * int as parameter and updates the value it points to to 98.
+ * reset_to_98 - Cambia la direccion de mememoria 98
  * @n: integer
  * Return: void
  */
