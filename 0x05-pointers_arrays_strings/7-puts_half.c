d4a209bdae9aa70633912d9b4d158a1ecaa761b3
UCIX210 <javier.uc@outlook.com>
2022-02-14 20:51:07 -0600

    0x05 7.4

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,6 +7,7 @@ void puts_half(char *str)
 {
 	int i;
 	int o;
+
 	i = 0;
 	while (str[i] != 0)
 	{
@@ -14,14 +15,14 @@ void puts_half(char *str)
 	}
 	if (i % 2 != 0)
 	{
-		for (o = (i + 2) / 2;str[o] != 0; o++)
+		for (o = (i + 2) / 2; str[o] != 0; o++)
 		{
 			_putchar(str[o]);
 		}
 	}
 	else
 	{
-		for (o = (i + 1) / 2;str[o] != 0; o++)
+		for (o = (i + 1) / 2; str[o] != 0; o++)
 		{
 			_putchar(str[o]);
 		}
