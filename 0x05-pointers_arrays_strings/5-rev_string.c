faf54c067ef246fc9c2bd11eb4f4cf60d34f0dbc
UCIX210 <javier.uc@outlook.com>
2022-02-14 20:07:35 -0600

    0x05 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int o;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	o = i - 1;
+	while (o >= 0)
+	{
+		_putchar(s[o]);
+		o--;
+	}
+	_putchar('\n');
+}
