883d8cdfbc5bdbe20e42d92f73e35144f7766a8c
UCIX210 <javier.uc@outlook.com>
2022-02-14 20:56:35 -0600

    0x05 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest : pointer  buffer 
+ * @src : string
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, o;
+
+	i = 0;
+	while (src[i] != 0)
+	{
+		i++;
+	}
+	for (o = 0; o < i; o++)
+	{
+		dest[o] = src[o];
+	}
+	dest[o] = 0;
+	return (dest);
+}
