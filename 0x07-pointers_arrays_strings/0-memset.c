2b5ace0abc1b9a0d54a12b6840e6dbab0c45634e
UCIX210 <javier.uc@outlook.com>
2022-02-22 21:41:39 -0600

    0x07 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
- * _memset - s
+ * _memset - Write a function that fills memory with a constant byte.
  *
- * @s: s
- * @b: s
- * @n: s
+ * @s: string a llenar con b
+ * @b: Sting que llena n s
+ * @n: numero de veces que se escribe
  *
  * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+return (s);
 }
