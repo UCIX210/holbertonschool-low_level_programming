c275a3bbe40015faab8e87e0b813010c125ae6a9
UCIX210 <javier.uc@outlook.com>
2022-02-23 19:03:28 -0600

    0x08 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
-
 /**
- * _print_rev_recursion -prints a string in reverse followed by a new line
+ * _print_rev_recursion - prints a string in reverse.
  *
  * @s: string
  *
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
