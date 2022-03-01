8816d2669e808f6f658658e2e32b969c6b48643d
UCIX210 <javier.uc@outlook.com>
2022-03-01 17:06:25 -0600

    0x0a 3.8

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,10 @@ int main(int argc, char *argv[])
 {
 	int answer;
 		if (argc == 3)
-		{	answer = atoi(argv[1]) * atoi(argv[2]);
-				printf("%d\n", answer);
-				return (0);
+		{	
+			answer = atoi(argv[1]) * atoi(argv[2]);
+			printf("%d\n", answer);
+			return (0);
 		}
 		else
 		{
