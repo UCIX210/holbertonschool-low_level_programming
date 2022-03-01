950cd1c9d6e5a5076a7f982e309994c1c8153759
UCIX210 <javier.uc@outlook.com>
2022-03-01 17:50:53 -0600

    0x0a 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
  * main - program that adds 2 numbers
  * @argc: arguments added together to get answer
@@ -8,5 +10,30 @@
  */
 int main(int argc, char *argv[])
 {
+	int i;
+	int o;
+	int suma;
 
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (o = 0; argv[i][o]; o++)
+			{
+				if (!isdigit(argv[i][o]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			suma = suma + atoi(argv[i]);
+		}
+		printf("%d\n", suma);
+	}
+	else
+	{
+		printf("0\n");
+		return (1);
+	}
+	return (0);
 }
