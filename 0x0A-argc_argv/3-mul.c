2c96b15b4c1a09e9294d0b469a37048b958a6f57
UCIX210 <javier.uc@outlook.com>
2022-03-01 15:30:11 -0600

    0x0a 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints the multiplication answer on a new line
  * @argc: argc = answer
@@ -8,5 +9,18 @@
  */
 int main(int argc, char *argv[])
 {
+	if (argc == 3)
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+		int c = a * b;
 
+		printf("%d \n", c);
+	}
+	else
+	{
+		printf("Error \n");
+		return (1);
+	}
+	return (0);
 }
