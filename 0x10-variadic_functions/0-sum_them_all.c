dba124415bfdd4b0b2694842549c7c77c139c261
UCIX210 <javier.uc@outlook.com>
2022-03-10 15:09:52 -0600

    0x10 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum a variable amount of given values
+ * @n: the identifier for the arguements
+ * Return: the sum of all the variables
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = 0;
+	int sum = 0;
+	va_list num;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(num, n);
+	for (i = 0; i < n; ++i)
+	{
+		sum += va_arg(num, int);
+	}
+	va_end(num);
+	return (sum);
+}
