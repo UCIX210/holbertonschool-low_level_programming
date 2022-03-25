9f03b57d63e3e842329319f2f673873fbceaa2a8
UCIX210 <javier.uc@outlook.com>
2022-03-24 20:53:19 -0600

    0x14 2.2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * get_bit -  función que devuelva el valor de un bit en un índice dado
- * @val: el valor para examinar el bit
+ * get_bit - función que devuelva el valor de un bit en un índice dado
+ * @n: el valor para examinar el bit
  * @index: el índice del bit a comprobar
- * Returns:  el valor del bit en el índice de índice o -1 si se produjo un error
+ * Return: el valor del bit o -1 si se produjo un error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
