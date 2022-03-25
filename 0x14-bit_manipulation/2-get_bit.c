17852da6a82c847e65d8a427cd40e46384ece002
UCIX210 <javier.uc@outlook.com>
2022-03-24 20:49:33 -0600

    0x14 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * get_bit - find the value of a bit at a certain index in val
+ * @val: the value to examine the bit
+ * @index: the index of the bit to check
+ * Return: the bit set at index of val or -1 if failed.
+ */
+int get_bit(unsigned long int val, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	return (1 & (val >> index));
+}
