5978799ae6049e99a10922d9d4a9af02a61d87c7
UCIX210 <javier.uc@outlook.com>
2022-03-24 20:59:18 -0600

    0x14 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * flip_bits - función que devuelva la cantidad de bits que necesitaría cambiar para pasar de un número a otro
- * @num1: Primer valor a comprar
- * @num2: Segundo valor a comprar
+ * flip_bits - función que devuelva la cantidad de bits
+ * @n: Primer valor a comprar
+ * @m: Segundo valor a comprar
  * Return: contador
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
