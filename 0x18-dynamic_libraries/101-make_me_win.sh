d3eff7e82367d6faf143572385e665abaf30d44c
UCIX210 <javier.uc@outlook.com>
2022-05-09 23:41:14 -0500

    0x018 0.10

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /home/vagrant/holberton/holbertonschool-low_level_programming/0x18-dynamic_libraries  https://github.com/ffelipegupe/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libfoo.so
-export LD_PRELOAD=/home/vagrant/holberton/holbertonschool-low_level_programming/0x18-dynamic_libraries/libfoo.so
